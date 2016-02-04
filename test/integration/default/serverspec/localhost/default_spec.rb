a0f5c8d357956e8556e9f51b70947d181d6a1e39
AsIf <iamthe.angrytoast@gmail.com>
2016-02-03 16:12:51 -0800

    add tests. update pkg to name

---
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'ansible-logstash::default' do
+
+  describe package('logstash') do
+    it { should be_installed.by('apt') }
+  end
+
+  describe file('/var/log/logstash') do
+    it { should be_directory }
+    it { should be_owned_by 'logstash' }
+    it { should be_grouped_into 'logstash' }
+    it { should be_mode 775 }
+  end
+
+  describe file('/etc/init/logstash.conf') do
+    it { should exist }
+  end
+
+  describe service('logstash') do
+    it { should be_installed }
+    it { should be_running }
+  end
+
+end
